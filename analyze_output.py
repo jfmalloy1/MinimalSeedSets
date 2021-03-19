95235f168a6d16d8bde7004c7ffa9aed5eedc1b3
John F. Malloy <jfmalloy1@gmail.com>
2021-03-19 11:27:54 -0700

    Finding minimal seeds for random test

---
+++ analyze_output.py
@@ -0,0 +1,35 @@
+import json
+import pandas as pd
+from tqdm import tqdm
+import os
+
+""" Finds minimal seeds (scope_seeds) and size of OG seed set (scope_compounds)
+    Input: filepath to json filepath
+    Output: pandas series which can be added to an existing dataframe
+"""
+def analyze_json(fp):
+    data = json.load(open(fp))
+    #Return relevant data
+    return pd.Series({"Seed Size": len(data["stats"]["scope_compounds"]),
+        "MSS Size": len(data["stats"]["scope_seeds"]),
+        "MSS Compounds": data["stats"]["scope_seeds"]})
+
+def main():
+    # #Test
+    # test_series = analyze_json("output/mw3300036539/output.json")
+    # df = df.append(test_series, ignore_index=True)
+    # print(df)
+
+    #Set up dataframe
+    df = pd.DataFrame(columns=["Seed Size", "MSS Size", "MSS Compounds"])
+    #Sorting by molecular weight results
+    for dir in tqdm(os.listdir("output")):
+        if dir.startswith("mw"):
+            test_series = analyze_json("output/" + dir + "/output.json")
+            df = df.append(test_series, ignore_index=True)
+
+    print(df)
+    df.to_csv("output/MSSresults_mw.csv")
+
+if __name__ == "__main__":
+    main()
